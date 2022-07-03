854f5bb0402eed537b2f2f6ea1c7b70881d40023
minjun-P <jun7332568@gmail.com>
2022-07-03 21:03:06 +0900

    getx_screen 초안 완성

---
+++ getx_screen/__brick__/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}Screen extends GetView<{{name.pascalCase()}}ScreenController> {
+  const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ getx_screen/__brick__/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/instance_manager.dart';
+import '{{name.snakeCase()}}_screen_controller.dart';
+
+class {{name.pascalCase()}}ScreenBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.put({{name.pascalCase()}}ScreenController();
+  }
+}
