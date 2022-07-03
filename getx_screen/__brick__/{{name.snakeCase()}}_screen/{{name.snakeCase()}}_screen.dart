eec9d37a0f14697ce5cc2fc91beeb6de48f241ee
minjun-P <jun7332568@gmail.com>
2022-07-03 21:10:53 +0900

    getx 스크린 오류 수정2

--- getx_screen/__brick__/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen.dart
+++ getx_screen/__brick__/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 
+import '{{name.snakeCase()}}_screen_controller.dart';
+
 class {{name.pascalCase()}}Screen extends GetView<{{name.pascalCase()}}ScreenController> {
   const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
 
