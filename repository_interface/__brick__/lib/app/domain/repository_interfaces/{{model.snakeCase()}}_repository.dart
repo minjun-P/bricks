bcbbc61cdcc5184d49a485262b19d96e69cad825
minjun-P <jun7332568@gmail.com>
2022-07-03 22:56:11 +0900

    repository interface 추가

---
+++ repository_interface/__brick__/lib/app/domain/repository_interfaces/{{model.snakeCase()}}_repository.dart
@@ -0,0 +1,16 @@
+abstract class {{model.pascalCase()}}Repository{
+  
+  // Future createById();
+
+  // Future create();
+
+  // Future readById();
+
+  // Future readAll();
+
+  // Future updateById();
+
+  // Future deleteById();
+
+  // Future deleteAll(); 
+}
