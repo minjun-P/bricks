6fdaa71dec03a75db28e7b849e28174ca58c9a66
hanjoongyoo <hanjoongyoo@naver.com>
2022-07-05 21:44:25 +0900

    Repository Interface 수정

--- repository_interface/__brick__/lib/app/domain/repository_interfaces/{{model.snakeCase()}}_repository.dart
+++ repository_interface/__brick__/lib/app/domain/repository_interfaces/{{model.snakeCase()}}_repository.dart
@@ -1,16 +1,21 @@
+import '../models/{{model.snakeCase()}}.dart';
+
 abstract class {{model.pascalCase()}}Repository{
-  
-  // Future createById();
 
-  // Future create();
+  // Future<String> createWithId({{model.pascalCase}} {{model.camelCase()}}, String id);
 
-  // Future readById();
+  // Future<String> create({{model.pascalCase()}} {{model.camelCase()}});
 
-  // Future readAll();
+  // Future<{{model.pascalCase()}}> readById(String id);
 
-  // Future updateById();
+  // 이건 그냥 지침 사항, Query 대신에 어떤 id인지 기입하여 사용
+  // Future<List<{{model.pascalCase()}}>> readByQueryId(String queryId);
 
-  // Future deleteById();
+  // Future<List<{{model.pascalCase()}}>> readAll();
 
-  // Future deleteAll(); 
+  // Future<void> updateById({{model.pascalCase}} {{model.camelCase()}}, String id);
+
+  // Future<void> deleteById(String id);
+
+  // Future<void> deleteAll();
 }
