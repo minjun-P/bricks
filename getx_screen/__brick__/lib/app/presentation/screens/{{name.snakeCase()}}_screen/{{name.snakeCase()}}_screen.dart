478328c2311e94576b094d4561f357678041c570
minjun-P <jun7332568@gmail.com>
2022-07-21 13:19:15 +0900

    getx 스크린, 겟라우트 부분 수정

--- getx_screen/__brick__/lib/app/presentation/screens/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen.dart
+++ getx_screen/__brick__/lib/app/presentation/screens/{{name.snakeCase()}}_screen/{{name.snakeCase()}}_screen.dart
@@ -2,13 +2,20 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 
-import '{{name.snakeCase()}}_screen_controller.dart';
 
 class {{name.pascalCase()}}Screen extends GetView<{{name.pascalCase()}}ScreenController> {
   const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text('{{name.snakeCase()}}')
+    );
   }
 }
+
+GetPage(
+          name: ,
+          page: () => const {{name.pascalCase()}}Screen(),
+          binding: {{name.pascalCase()}}ScreenBinding(),
+        )
